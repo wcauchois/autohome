76b9e2aa4c869abeb69085305d41c4b3cb151c00
Bill Cauchois <wcauchois@gmail.com>
2013-12-29 13:47:55 -0500

    listen on all interfaces

--- avswitcher-web/main.py
+++ avswitcher-web/main.py
@@ -12,4 +12,4 @@ def hello():
 if __name__ == '__main__': 
   input_sources_json = json.load(open('inputsources.json', 'r'))
   input_sources_list = input_sources_json.keys()
-  app.run()
+  app.run(host='0.0.0.0')
