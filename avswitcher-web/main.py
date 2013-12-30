58ab5f63c123fb62f7b43da2510032f09766959c
Bill Cauchois <wcauchois@gmail.com>
2013-12-30 03:36:03 +0000

    IT WORKS@!!!

--- avswitcher-web/main.py
+++ avswitcher-web/main.py
@@ -1,23 +1,23 @@
 import json
 from flask import Flask, render_template, request
 import flask
-import threading
+from threading import Thread
 from Queue import Queue
-import os
+import os, sys, signal
 from time import sleep
 
 app = Flask(__name__)
 
 input_sources_list = []
 
-@app.route("/")
+@app.route('/')
 def hello():
   return render_template('index.html', inputsources=input_sources_list)
 
 def submit_lirc_command(code):
-  flask.g.lirc_command_queue.put(code)
+  lirc_command_queue.put(code)
 
-@app.route('switch_hdmi', methods=['POST'])
+@app.route('/switch_hdmi', methods=['POST'])
 def switch_hdmi():
   desired_source = request.form['source']
   if desired_source in map(str, range(1, 5)):
@@ -28,19 +28,30 @@ def switch_hdmi():
     
 
 def lirc_command_worker(q):
+  #return
   while True:
     code = q.get()
+    if code == 'EXIT':
+      break
     ret = os.system('irsend SEND_START my_remote %s' % code)
     if ret == 0:
       sleep(0.3)
       os.system('irsend SEND_STOP my_remote %s' % code)
 
+# We need to install an interrupt handler so that we can shut down
+# the worker thread and exit cleanly.
+def interrupt_handler(signal, frame):
+  lirc_command_queue.put('EXIT')
+  worker_thread.join()
+  sys.exit(0)
+signal.signal(signal.SIGINT, interrupt_handler)
+
 if __name__ == '__main__': 
   input_sources_json = json.load(open('inputsources.json', 'r'))
   input_sources_list = input_sources_json.keys()
 
-  flask.g.lirc_command_queue = Queue()
-  worker_thread = Thread(target=lirc_command_worker, args=(flask.g.lirc_command_queue,))
+  lirc_command_queue = Queue()
+  worker_thread = Thread(target=lirc_command_worker, args=(lirc_command_queue,))
   worker_thread.start()
 
   app.run(host='0.0.0.0')
